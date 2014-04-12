3ae0db8bc05467241d95cd84eb0d2254ee73adc6
java <deonisy@inbox.lv>
2014-04-12 13:00:32 +0300

    Rejected commit: Default

--- app/src/main/java/com/example/myapplication2/app/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/app/MainActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     public int a=0;
+    public int b=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +34,16 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.inc) {
+            b++;
+            Button button = (Button) findViewById(R.id.button);
+            button.setText(String.valueOf(b));
+            return true;
+        }
+        if (id == R.id.dec) {
+            b--;
+            Button button = (Button) findViewById(R.id.button);
+            button.setText(String.valueOf(b));
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -40,10 +51,15 @@ public class MainActivity extends Activity {
 
     public boolean Writer(View view)
     {
-      a++;
-      TextView wri = (TextView) findViewById(R.id.textView);
+      a=a+b;
+      TextView wri = (TextView) findViewById(R.id.wri);
       String tt =String.valueOf(a);
       wri.setText(tt);
+      if (a==1000)
+      {
+          TextView wintext = (TextView) findViewById(R.id.wintext);
+          wintext.setText("You WIN!!!");
+      }
       return true;
     }
 
